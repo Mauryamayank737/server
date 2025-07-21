62050c2e4d0e4590034b53cc201c022bc9fc2c90
Mauryamayank737 <mayank8355@gmail.com>
2025-07-21 15:13:07 +0530

    multer changes

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,24 +1,2 @@
-import multer from "multer";
-
-// In your multer.js middleware
-const storage = multer.memoryStorage();
-
-export const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
-  fileFilter: (req, file, cb) => {
-    // console.log('📥 File received:', {
-    //   name: file.originalname,
-    //   type: file.mimetype,
-    //   size: file.size
-    // });
-
-    if (['image/jpeg', 'image/png', 'image/webp'].includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type'));
-    }
-  },
-});
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
