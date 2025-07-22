4e45c2d6380ab982ed402022baa09b370581fb59
Mauryamayank737 <mayank8355@gmail.com>
2025-07-22 19:16:20 +0530

    frontend path correct

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://client-zepto.vercel.app","https://client-grocery.vercel.app/"],
+    origin: ["http://localhost:5173", "https://client-zepto.vercel.app","https://client-grocery.vercel.app"],
     credentials: true,
   })
 );
